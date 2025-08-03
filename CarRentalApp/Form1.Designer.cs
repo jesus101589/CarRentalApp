f2cd84b734459d3011409818b2c85e1b36dda1dc
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-08-03 17:53:44 -0500

    Connect SQL Database to the project

--- CarRentalApp/Form1.Designer.cs
+++ CarRentalApp/Form1.Designer.cs
@@ -46,17 +46,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(156, 9);
+            this.label1.Location = new System.Drawing.Point(234, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(444, 67);
+            this.label1.Size = new System.Drawing.Size(665, 101);
             this.label1.TabIndex = 0;
             this.label1.Text = "Car Rental System";
             // 
             // txtboxCustomerName
             // 
-            this.txtboxCustomerName.Location = new System.Drawing.Point(12, 160);
+            this.txtboxCustomerName.Location = new System.Drawing.Point(18, 246);
+            this.txtboxCustomerName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtboxCustomerName.Name = "txtboxCustomerName";
-            this.txtboxCustomerName.Size = new System.Drawing.Size(200, 20);
+            this.txtboxCustomerName.Size = new System.Drawing.Size(298, 26);
             this.txtboxCustomerName.TabIndex = 1;
             this.txtboxCustomerName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -64,33 +66,37 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 137);
+            this.label2.Location = new System.Drawing.Point(12, 211);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.Size = new System.Drawing.Size(188, 29);
             this.label2.TabIndex = 2;
             this.label2.Text = "Customer Name";
             // 
             // dtrented
             // 
-            this.dtrented.Location = new System.Drawing.Point(12, 247);
+            this.dtrented.Location = new System.Drawing.Point(18, 380);
+            this.dtrented.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dtrented.Name = "dtrented";
-            this.dtrented.Size = new System.Drawing.Size(200, 20);
+            this.dtrented.Size = new System.Drawing.Size(298, 26);
             this.dtrented.TabIndex = 3;
             // 
             // dtreturned
             // 
-            this.dtreturned.Location = new System.Drawing.Point(479, 247);
+            this.dtreturned.Location = new System.Drawing.Point(718, 380);
+            this.dtreturned.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dtreturned.Name = "dtreturned";
-            this.dtreturned.Size = new System.Drawing.Size(249, 20);
+            this.dtreturned.Size = new System.Drawing.Size(372, 26);
             this.dtreturned.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(8, 224);
+            this.label3.Location = new System.Drawing.Point(12, 345);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.Size = new System.Drawing.Size(147, 29);
             this.label3.TabIndex = 5;
             this.label3.Text = "Date Rented";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -99,9 +105,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(493, 224);
+            this.label4.Location = new System.Drawing.Point(740, 345);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.Size = new System.Drawing.Size(168, 29);
             this.label4.TabIndex = 6;
             this.label4.Text = "Date Returned";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -110,14 +117,10 @@
             // 
             this.cbTypeOfCar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbTypeOfCar.FormattingEnabled = true;
-            this.cbTypeOfCar.Items.AddRange(new object[] {
-            "Honda",
-            "Toyota",
-            "GMC ",
-            "Porsche"});
-            this.cbTypeOfCar.Location = new System.Drawing.Point(12, 316);
+            this.cbTypeOfCar.Location = new System.Drawing.Point(18, 486);
+            this.cbTypeOfCar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbTypeOfCar.Name = "cbTypeOfCar";
-            this.cbTypeOfCar.Size = new System.Drawing.Size(200, 21);
+            this.cbTypeOfCar.Size = new System.Drawing.Size(298, 28);
             this.cbTypeOfCar.TabIndex = 7;
             this.cbTypeOfCar.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -125,18 +128,20 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(8, 293);
+            this.label5.Location = new System.Drawing.Point(12, 451);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.Size = new System.Drawing.Size(138, 29);
             this.label5.TabIndex = 8;
             this.label5.Text = "Type of Car";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(574, 356);
+            this.button1.Location = new System.Drawing.Point(861, 548);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 51);
+            this.button1.Size = new System.Drawing.Size(290, 78);
             this.button1.TabIndex = 9;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
@@ -144,26 +149,28 @@
             // 
             // tbCost
             // 
-            this.tbCost.Location = new System.Drawing.Point(479, 160);
+            this.tbCost.Location = new System.Drawing.Point(718, 246);
+            this.tbCost.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbCost.Name = "tbCost";
-            this.tbCost.Size = new System.Drawing.Size(249, 20);
+            this.tbCost.Size = new System.Drawing.Size(372, 26);
             this.tbCost.TabIndex = 10;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(475, 137);
+            this.label6.Location = new System.Drawing.Point(712, 211);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(42, 20);
+            this.label6.Size = new System.Drawing.Size(62, 29);
             this.label6.TabIndex = 11;
             this.label6.Text = "Cost";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.tbCost);
             this.Controls.Add(this.button1);
@@ -176,6 +183,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtboxCustomerName);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
